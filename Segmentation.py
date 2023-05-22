d65be2e3ea984bcf04889761ffc2a89d62359be7
SY-ML <ai.engr.sy@gmail.com>
2023-05-22 20:21:26 +0900

    052223

--- Segmentation.py
+++ Segmentation.py
@@ -1,27 +1,36 @@
 import pandas as pd
-from datetime import datetime
+from datetime import datetime, timedelta
 from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
 
-df = pd.read_csv('./dataset/Sales Product Data/Sales_August_2019.csv')
+df = pd.read_csv('./dataset/Sales Product Data/Sales_April_2019.csv')
+df = df[df['Order Date'] != 'Order Date']
+def fix_double_period(val):
+    if isinstance(val, str):  # only apply the operation to string values
+        parts = val.split('.')
+        if len(parts) > 2:
+            return parts[0] + '.' + ''.join(parts[1:])
+    return val
+
+df['Price Each'] = df['Price Each'].apply(fix_double_period).astype(float)
 
 # Assuming df is your DataFrame and "Order Date" is in MM/DD/YY H:M format
 df['Order Date'] = pd.to_datetime(df['Order Date'], format='%m/%d/%y %H:%M')
 
-print(df)
-print(df.dtypes)
-exit()
 # Calculate Recency, Frequency and Monetary value for each customer
-snapshot_date = df['Order Date'].max() + datetime.timedelta(days=1) # latest date in the data set plus one day
+snapshot_date = df['Order Date'].max() + timedelta(days=1) # latest date in the data set plus one day
 df_RFM = df.groupby(['Order ID']).agg({
     'Order Date': lambda x: (snapshot_date - x.max()).days,
     'Product': 'count',
     'Price Each': 'sum'
 })
 
-# Rename columns
+
+# Rename the columns to Recency, Frequency and Monetary
 df_RFM.rename(columns = {'Order Date': 'Recency',
                          'Product': 'Frequency',
-                         'Price Each': 'MonetaryValue'}, inplace=True)
+                         'Price Each': 'Monetary'}, inplace=True)
+
 
 # Scaling is very important in KMeans
 from sklearn.preprocessing import StandardScaler
@@ -33,3 +42,27 @@ kmeans = KMeans(n_clusters=3, random_state=0).fit(df_RFM_normalized)
 
 # Append the KMeans clustering result back to the RFM data frame
 df_RFM['Cluster'] = kmeans.labels_
+
+print(df_RFM['Cluster'].value_counts())
+print(df_RFM)
+# Prepare your figure and axes
+fig = plt.figure(figsize=(10, 8))
+ax = fig.add_subplot(111, projection='3d')
+
+# Prepare a color map
+cmap = plt.cm.get_cmap("viridis")
+
+# Create a scatter plot
+sc = ax.scatter(df_RFM['Recency'], df_RFM['Frequency'], df_RFM['Monetary'], s=50, c=df_RFM['Cluster'], cmap=cmap)
+
+# Create a colorbar
+cb = plt.colorbar(sc, ax=ax)
+
+# Set colorbar title
+cb.set_label('Cluster')
+
+ax.set_xlabel('Recency')
+ax.set_ylabel('Frequency')
+ax.set_zlabel('Monetary')
+plt.title('3D scatter plot of RFM data')
+plt.show()
