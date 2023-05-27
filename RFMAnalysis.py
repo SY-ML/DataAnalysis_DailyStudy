06f371e2b6a942c6fe96bad1a1d8c1a1381ce293
SY-ML <ai.engr.sy@gmail.com>
2023-05-27 23:31:04 +0900

    052723

--- RFMAnalysis.py
+++ RFMAnalysis.py
@@ -3,6 +3,7 @@ from datetime import datetime, timedelta
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
+import numpy as np
 
 import statistics
 
@@ -10,8 +11,9 @@ import statistics
 # https://datarian.io/blog/what-is-rfm
 class RFMAnalysis():
     def __init__(self, df, col_date, col_customer, col_product, col_order, col_qty, col_price):
+        # Save the input parameters as instance variables
         self._df = df
-        self._snapshot_date = df[col_date].max() + timedelta(days=1)
+        self._snapshot_date = df[col_date].max() + timedelta(days=1)  # latest date in the data set plus one day
 
         self._col_date = col_date
         self._col_customer = col_customer
@@ -25,72 +27,87 @@ class RFMAnalysis():
         self.df_prcd = self.preprocess_dataset()
 
     def preprocess_dataset(self):
+        # Copy the required columns into a new DataFrame
         df = self._df[self._ls_cols_in_use].copy()
 
+        # Convert the date column to datetime format
         df[self._col_date] = pd.to_datetime(df[self._col_date], infer_datetime_format=True, dayfirst=True)
 
+        # Drop NA values and duplicates
         df.dropna(inplace=True)
         df.drop_duplicates(inplace=True)
 
+        # Compute sales as product of price and quantity
         df['sales'] = df[self._col_price] * df[self._col_qty]
         df.drop(columns=[self._col_price, self._col_qty])
 
+        # Group by customer ID and compute recency, frequency and monetary value for each customer
         df_RFM = df.groupby([self._col_customer]).agg({
             self._col_date: lambda x: (self._snapshot_date - x.max()).days,
-            self._col_product: 'nunique',  # Number of unique products per customer
+            self._col_product: 'nunique',
             'sales': 'sum'})
-
-        df_RFM.rename(columns={self._col_date: 'Recency', self._col_product: 'Frequency', 'sales': 'Monetary'},
-                      inplace=True)
 
         return df_RFM
 
     def find_optimal_clusters(self, data, max_k):
+        # Calculate SSE for different number of clusters
         iters = range(2, max_k + 1)
-
         sse = []
         for k in iters:
             sse.append(KMeans(n_clusters=k, random_state=0).fit(data).inertia_)
 
+        # Calculate the line connecting the first and last SSE
         p1 = np.array([2, sse[0]])
         p2 = np.array([max_k, sse[-1]])
 
+        # Calculate the distance of each SSE to the line
         distances = []
         for i, sse_i in enumerate(sse):
             p = np.array([i + 2, sse_i])
             distance = np.abs(np.cross(p2 - p1, p1 - p)) / np.linalg.norm(p2 - p1)
             distances.append(distance)
 
+        # Find the index of the maximum distance
         optimal_k = distances.index(max(distances)) + 2
 
         return optimal_k
 
-    def run_kmeans(self, max_k=10):
+    def run_kmeans(self, max_k=None):
         df_RFM = self.df_prcd
 
+        # Scaling is very important in KMeans
         scaler = StandardScaler()
         df_RFM_normalized = scaler.fit_transform(df_RFM)
 
-        optimal_k = self.find_optimal_clusters(df_RFM_normalized, max_k)
+        # Find the optimal number of clusters if not specified
+        if max_k is None:
+            max_k = self.find_optimal_clusters(df_RFM_normalized, 10)
 
-        kmeans = KMeans(n_clusters=optimal_k, random_state=0).fit(df_RFM_normalized)
+        # Perform KMeans clustering with the optimal number of clusters
+        kmeans = KMeans(n_clusters=max_k, random_state=0).fit(df_RFM_normalized)
 
+        # Append the KMeans clustering result back to the RFM data frame
         df_RFM['Cluster'] = kmeans.labels_
 
         return df_RFM
 
     def visualize_clusters(self):
         df_RFM = self.df_prcd
+        # Prepare your figure and axes
         fig = plt.figure(figsize=(10, 8))
         ax = fig.add_subplot(111, projection='3d')
 
+        # Prepare a color map
         cmap = plt.cm.get_cmap("viridis")
 
+        # Create a scatter plot
         sc = ax.scatter(df_RFM['Recency'], df_RFM['Frequency'], df_RFM['Monetary'], s=50, c=df_RFM['Cluster'],
                         cmap=cmap)
 
+        # Create a colorbar
         cb = plt.colorbar(sc, ax=ax)
 
+        # Set colorbar title
         cb.set_label('Cluster')
 
         ax.set_xlabel('Recency')
@@ -98,4 +115,3 @@ class RFMAnalysis():
         ax.set_zlabel('Monetary')
         plt.title('3D scatter plot of RFM data')
         plt.show()
-
