01cd6824292df9af5966b61add524d0b8c695540
SY-ML <ai.engr.sy@gmail.com>
2023-06-30 07:23:27 +0900

    063023

--- VIsualizer.py
+++ VIsualizer.py
@@ -1,5 +1,7 @@
 import plotly.express as px
 import pandas as pd
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
 
 # Define the data
 df = pd.DataFrame([
@@ -9,6 +11,31 @@ df = pd.DataFrame([
     dict(UserID='User 2', Activity='Activity 2', Start='2023-01-04', Finish='2023-01-05'),
 ])
 
-# Create a Gantt chart
-fig = px.timeline(df, x_start='Start', x_end='Finish', y='UserID', color='Activity')
+def visualize_in_gantt_chart(df, col_xstart, col_xend, col_y, col_val):
+    # Create a Gantt chart
+    fig = px.timeline(df, x_start=col_xstart, x_end=col_xend, y=col_y, color= col_val)
+    fig.show()
+
+
+visualize_in_gantt_chart(df, 'Start', 'Finish', 'UserID', 'Activity')
+
+exit()
+# Create the subplots
+fig = make_subplots(rows=2, cols=1)
+
+# Add the overall Gantt chart
+fig.add_trace(
+    go.Bar(x=df['Start'], y=df['Activity'], name='Overall'),
+    row=1, col=1
+)
+
+# Add the individual user timelines
+for user in df['UserID'].unique():
+    df_user = df[df['UserID'] == user]
+    fig.add_trace(
+        go.Bar(x=df_user['Start'], y=df_user['Activity'], name=user),
+        row=2, col=1
+    )
+
+fig.update_layout(height=600, width=800, title_text="Overall and User Timelines")
 fig.show()
