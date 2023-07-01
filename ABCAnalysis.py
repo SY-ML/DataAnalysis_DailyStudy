4f141808d785f3fa674cb5d6276587c1085f1271
SY-ML <ai.engr.sy@gmail.com>
2023-07-01 23:05:28 +0900

    070123

--- ABCAnalysis.py
+++ ABCAnalysis.py
@@ -1,3 +1,33 @@
-print(1)
 
-#062623 :(
+import  pandas as pd
+
+
+
+# data structure
+
+
+# ABC analysis - frequency.
+"""
+AP - Order qty varies by season?
+EQ - Order qty varies by season?
+FW - Order qty varies by season? 
+
+
+On what basis the classification for abc analysis should be performed on?
+- yearly?
+- semi-yearly?
+- quarterly?
+- monthly?
+
+
+What criteria should be applied when dividing items into group
+- reorder rate?
+- total order quantity in the given period?
+- 일재고량 / 일 출고량 비율 (또는 4 사분면)
+
+
+
+- cost per unit
+- 
+
+"""
