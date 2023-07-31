e64c65d44daf8f1131061cf9d28734583787d7a4
SY-ML <ai.engr.sy@gmail.com>
2023-08-01 06:57:12 +0900

    080123

--- VIsualizer.py
+++ VIsualizer.py
@@ -1,44 +1,42 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
 import plotly.express as px
 import pandas as pd
 from plotly.subplots import make_subplots
 import plotly.graph_objects as go
 
 # Define the data
-df = pd.DataFrame([
-    dict(UserID='User 1', Activity='Activity 1', Start='2023-01-01', Finish='2023-01-02'),
-    dict(UserID='User 1', Activity='Activity 2', Start='2023-01-02', Finish='2023-01-03'),
-    dict(UserID='User 2', Activity='Activity 1', Start='2023-01-03', Finish='2023-01-04'),
-    dict(UserID='User 2', Activity='Activity 2', Start='2023-01-04', Finish='2023-01-05'),
-])
+
+def load_data_sample():
+    df = pd.DataFrame([
+        dict(UserID='User 1', Activity='Activity 1', Start='2023-01-01', Finish='2023-01-02'),
+        dict(UserID='User 1', Activity='Activity 2', Start='2023-01-02', Finish='2023-01-03'),
+        dict(UserID='User 2', Activity='Activity 1', Start='2023-01-03', Finish='2023-01-04'),
+        dict(UserID='User 2', Activity='Activity 2', Start='2023-01-04', Finish='2023-01-05'),
+    ])
+    return df
+
 
 def visualize_in_gantt_chart(df, col_xstart, col_xend, col_y, col_val):
     # Create a Gantt chart
     fig = px.timeline(df, x_start=col_xstart, x_end=col_xend, y=col_y, color= col_val)
     fig.show()
 
+df = pd.read_csv('./dataset/Customer Shopping Dataset - Retail Sales Data/customer_shopping_data.csv')
+df['year'] = pd.to_datetime(df['invoice_date'], format='%d/%m/%Y').dt.year
+def visualize_in_kdeplot(df, col_x, title, col_hue=None):
 
-visualize_in_gantt_chart(df, 'Start', 'Finish', 'UserID', 'Activity')
+    plt.figure(figsize = (10, 6))
 
-# Create the subplots
-fig = make_subplots(rows=2, cols=1)
+    if col_hue == None:
+        sns.kdeplot(df[col_x])
+    else:
+        for by_val in df[col_hue].unique():
+            # sns.histplot(df[df[col_hue] == by_val][col_x], label = by_val)
+            sns.kdeplot(df[df[col_hue] == by_val][col_x], label = by_val)
+            plt.legend(title = col_x)
 
-# Add the overall Gantt chart
-fig.add_trace(
-    go.Bar(x=df['Start'], y=df['Activity'], name='Overall'),
-    row=1, col=1
-)
+    plt.title(title)
+    plt.show()
 
-# Add the individual user timelines
-for user in df['UserID'].unique():
-    df_user = df[df['UserID'] == user]
-    fig.add_trace(
-        go.Bar(x=df_user['Start'], y=df_user['Activity'], name=user),
-        row=2, col=1
-    )
-
-fig.update_layout(height=600, width=800, title_text="Overall and User Timelines")
-fig.show()
-
-
-def visualize_in_boxplot(df, col):
-    return None
+visualize_in_kdeplot(df = df, col_x= 'quantity', title='Quantity Distribution by year', col_hue='year')
