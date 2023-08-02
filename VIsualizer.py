380858ff31d5ad55fb1dbeb5b8ac5b7f3332bc6d
SY-ML <ai.engr.sy@gmail.com>
2023-08-02 22:39:53 +0900

    080223

--- VIsualizer.py
+++ VIsualizer.py
@@ -40,3 +40,39 @@ def visualize_in_kdeplot(df, col_x, title, col_hue=None):
     plt.show()
 
 visualize_in_kdeplot(df = df, col_x= 'quantity', title='Quantity Distribution by year', col_hue='year')
+
+
+def late_clock_out_report(df):
+    import plotly.graph_objs as go
+    from plotly.subplots import make_subplots
+    import plotly.io as pio
+
+    # Assume the dataframe df is already in scope
+    # and 'date', 'user_id', 'last_clockout_time' are the relevant columns
+
+    df['hour'] = df['last_clockout_time'].dt.hour + df['last_clockout_time'].dt.minute / 60
+    df['wasted_time'] = 18 - df['hour']
+
+    # Create cumulative wasted time graph
+    df_cumulative = df.groupby('date')['wasted_time'].sum().cumsum().reset_index()
+    cumulative_wasted_time = go.Scatter(x=df_cumulative['date'], y=df_cumulative['wasted_time'], mode='lines',
+                                        name='Cumulative Wasted Time')
+
+    # Create average wasted time per operator graph
+    df_operator = df.groupby('user_id')['wasted_time'].mean().reset_index()
+    average_wasted_time = go.Bar(x=df_operator['user_id'], y=df_operator['wasted_time'], name='Average Wasted Time')
+
+    # Create histogram of clockout times
+    histogram_end_times = go.Histogram(x=df['hour'], nbinsx=24, name='Histogram of Clock Out Times')
+
+    # Add all plots to a subplot
+    fig = make_subplots(rows=3, cols=1)
+    fig.add_trace(cumulative_wasted_time, row=1, col=1)
+    fig.add_trace(average_wasted_time, row=2, col=1)
+    fig.add_trace(histogram_end_times, row=3, col=1)
+
+    # Update layout for better viewing
+    fig.update_layout(height=900, width=1200, title_text="Time Wasted Analysis", showlegend=True)
+
+    # Export to html
+    pio.write_html(fig, 'time_wasted_analysis.html')
